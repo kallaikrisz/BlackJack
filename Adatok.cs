ec90be00e56caa296268134fc5e600c483a0415f
Huss0003 <valuskatab@gmail.com>
2026-02-26 14:17:28 +0100

    adatok, txt és kártyaérték

---
+++ Adatok.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+public class Adatok
+{
+	public string Nev { get; set; }
+	public int Tet { get; set; }
+	public int Egyenleg { get; set; }
+}
+
+public class txtbe
+{
+    public void pontmentes(List<Adatok> jatekosok)
+	{
+		using (StreamWriter pontok = new StreamWriter("pontok.txt"))
+		{
+			foreach (int jatekosokSzama in JatekosSzam)
+			{
+				pontok.WriteLine($"{jatekos.Nev}; {jatekos.Tet}");
+            }
+        }
+	}
+}
