1091e6298efcb40cb31d148d241d8cd319a756f9
Huss0003 <valuskatab@gmail.com>
2026-02-27 10:44:52 +0100

    mukod2

--- Adatok.cs
+++ Adatok.cs
@@ -1,25 +1,31 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using BlackJack;
+using System.Windows;
+using System.Linq;
 
 
 public class Adatok
 {
-	public string Nev { get; set; }
-	public int Tet { get; set; }
-	public int Egyenleg { get; set; }
+    public string Nev { get; set; }
+    public int Tet { get; set; }
+    public int Egyenleg { get; set; }
+    public int pont { get; set; }
 }
 
 public class txtbe
 {
+    DateTime most = DateTime.Now;
     public void pontmentes(List<Adatok> jatekosok)
-	{
-		using (StreamWriter pontok = new StreamWriter("pontok.txt"))
-		{
-			foreach (Adatok jatekos in jatekosok)
-			{
-				pontok.WriteLine($"{jatekos.Nev}; {jatekos.Tet}");
+    {
+        int jatekosokszama = ((App)Application.Current).jatekosAdatok.Count;
+        using (StreamWriter pontok = new StreamWriter("pontok.txt"))
+        {
+            foreach (Adatok jatekos in jatekosok.Take(jatekosokszama))
+            {
+                pontok.WriteLine($"{most};{jatekos.Nev};{jatekos.Egyenleg}");
             }
         }
-	}
+    }
 }
