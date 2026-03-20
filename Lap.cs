e3a79be038cd6e7c3886f42f0d3de9a5d020139e
VendLNotFound <vevehun@gmail.com>
2026-03-20 09:32:03 +0100

    tisztogatas

---
+++ Lap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack.dokumentumok
+{
+     public class Lap
+    {
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public int Ertek { get; set; }
+        public Lap(int id, string nev, int ertek)
+        {
+            Id = id;
+            Nev = nev;
+            Ertek = ertek;
+        }
+    }
+}
